998e0d0ff9957d4e1e1317caddcd21bebb5ff328
Peter5793 <masaba.peter68@gmail.com>
2020-12-22 19:40:54 +0300

    Add the header file

---
+++ monty.h
@@ -0,0 +1,71 @@
+#ifndef MONTY_H
+#define MONTY_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#define Buffsize 30
+/* Global variables */
+extern int value[2];
+
+/* Structures */
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+  int n;
+  struct stack_s *prev;
+  struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+  char *opcode;
+  void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/* Fucntions */
+int value[2];
+int is_num(char *token);
+void _push(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
+void _pall(stack_t **stack, unsigned int line_number);
+void _swap(stack_t **stack, unsigned int line_number);
+void _pint(stack_t **stack, unsigned int line_number);
+void _nop(stack_t **stack, unsigned int line_number);
+void _pchar(stack_t **stack, unsigned int line_number);
+void _pstr(stack_t **stack, unsigned int line_number);
+
+/* Operation Functions */
+void _add(stack_t **stack, unsigned int line_number);
+void _sub(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void _mod(stack_t **stack, unsigned int line_number);
+void _rotr(stack_t **stack, unsigned int line_number);
+void _rotl(stack_t **stack, unsigned int line_number);
+
+stack_t *add_dnodeint_end(stack_t **head, int n);
+int delete_dnodeint_at_index(stack_t **head, unsigned int idx);
+void free_dlistint(stack_t *head);
+void simple_err(int del_coden, char *arv);
+void free_err(FILE *file, char *bff, stack_t *stk, int lne, int cde, char *tok);
+int strcmp_num(char *str);
+#endif
